1b42cf9c303d821c9939c32982225008378385c6
sokratismantes <167618740+sokratismantes@users.noreply.github.com>
2026-05-20 20:13:57 +0300

    Delete config.secret.php

--- config.secret.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-  'GEMINI_API_KEY' => 'AIzaSyCDo3tvi8PwAfMJbc7SJ9kmRH0IgIfp6HI',
-  'DB_HOST' => '127.0.0.1',
-    'DB_PORT' => '3306',
-    'DB_NAME' => 'diploma',
-    'DB_USER' => 'root',
-    'DB_PASS' => ''
-];
